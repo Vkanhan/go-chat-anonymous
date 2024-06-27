7ee02c345192b92e23da1f6f0a2c2347360caf2f
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-06-27 20:13:26 +0530

    refactor: refactor server logic
    - Extracted the code into seperate functions
    - Updated handleMessages functions for readability and maintainability

--- server.go
+++ server.go
@@ -12,52 +12,76 @@ var (
 	messageChan = make(chan string)          // Channel for incoming messages
 )
 
-// handleMessages manages incoming clients, departing clients, and messages
+// handleMessages manages incoming clients, disconnecting clients, and messages
 func handleMessages() {
 	for {
 		select {
 		case client := <-clientChan:
-			fmt.Printf("New client connected: %s\n", client.name)
-			// Check if the chat room (identified by client.passkey) already exists
-			if _, ok := clients[client.passkey]; !ok {
-				// If the chat room does not exist create a new slice and add the client
-				clients[client.passkey] = []*Client{client}
-			} else {
-				// If the chat room already exists, append the client to the existing slice
-				clients[client.passkey] = append(clients[client.passkey], client)
-			}
-			// Broadcast join message to the chat room
-			go broadcastToChat(fmt.Sprintf("%s has joined", client.name), client.passkey)
+			handleNewClient(client)
 		case client := <-leaveChan:
-			// Check if there are clients associated with the client's passkey
-			if clientList, ok := clients[client.passkey]; ok {
-				// Remove the client from the chat room
-				for i, c := range clientList {
-					// Find the client that is leaving
-					if c == client {
-						// Remove the client from the chat room slice
-						clients[client.passkey] = append(clientList[:i], clientList[i+1:]...)
-						break
-					}
-				}
-				// Broadcast leave message to the chat room
-				go broadcastToChat(fmt.Sprintf("%s has left", client.name), client.passkey)
-			}
+			handleClientLeave(client)
 		case message := <-messageChan:
-			// Split the message into passkey and content
-			// The message string is expected to be in the format "passkey|name: message"
-			// Splitting the string with a limit of 2 ensures that only the first occurrence of '|' is used for splitting
-			parts := strings.SplitN(message, "|", 2)
-			if len(parts) < 2 {
-				continue
-			}
-			// Extract passkey and content from the parts array store
-			passkey := parts[0] // parts[0] contains the passkey, which identifies the chat room
-			content := parts[1] // parts[1] contains the rest of the message to be broadcasted
-			// Broadcast message to the specific chat room
-			go broadcastMessage(passkey, content)
+			handleMessage(message)
 		}
 	}
+}
+
+// handleNewClient handles a newly connected client.
+func handleNewClient(client *Client) {
+	fmt.Printf("New client connected: %s\n", client.name)
+	addClientToChatRoom(client)
+	broadcastToChat(fmt.Sprintf("%s has joined", client.name), client.passkey)
+}
+
+// handleNewClient processes a newly connected client.
+func handleClientLeave(client *Client) {
+	if removeClientFromChatRoom(client) {
+		broadcastToChat(fmt.Sprintf("%s has left", client.name), client.passkey)
+	}
+}
+
+// handleMessage handles an incoming message from a client.
+func handleMessage(message string) {
+	// Split the message into passkey and content
+	// The message string is expected to be in the format "passkey|name: message"
+	// Splitting the string with a limit of 2 ensures that only the first occurrence of '|' is used for splitting
+	parts := strings.SplitN(message, "|", 2)
+	if len(parts) < 2 {
+		return
+	}
+	// Extract passkey and content from the parts array store
+	passkey := parts[0]	  // parts[0] contains the passkey, which identifies the chat room
+	content := parts[1]	 // parts[1] contains the the message to be broadcasted
+	broadcastMessage(passkey, content)
+}
+
+// addClientToChatRoom adds a client to the specified chat room.
+func addClientToChatRoom(client *Client) {
+	// Check if the chat room (identified by client.passkey) already exists
+	if _, ok := clients[client.passkey]; !ok {
+		// If the chat room does not exist create a new slice and add the client
+		clients[client.passkey] = []*Client{client}
+	} else {
+		// If the chat room already exists, append the client to the existing slice
+		clients[client.passkey] = append(clients[client.passkey], client)
+	}
+}
+
+// removeClientFromChatRoom removes a client from the specified chat room.
+func removeClientFromChatRoom(client *Client) bool {
+	// Check if there are clients associated with the client's passkey
+	if clientList, ok := clients[client.passkey]; ok {
+		// Remove the client from the chat room
+		for i, c := range clientList {
+			// Find the client that is leaving
+			if c == client {
+				// Remove the client from the chat room slice
+				clients[client.passkey] = append(clientList[:i], clientList[i+1:]...)
+				return true
+			}
+		}
+	}
+	return false
 }
 
 // broadcastMessage sends a message to all clients in the specified chat room
@@ -74,3 +98,4 @@ func broadcastMessage(passkey, message string) {
 func broadcastToChat(message, passkey string) {
 	broadcastMessage(passkey, message)
 }
+
